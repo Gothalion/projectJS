ab87c8f8b9d425c7b6530f70c65ccaf64c87e6fe
Gothalion <mbarsukovv@gmail.com>
2021-11-30 23:11:38 +0300

    Added: переменные, алерт и вывод в консоль

---
+++ script.js
@@ -0,0 +1,10 @@
+var title = "...";
+let screens = "...";
+let screenPrice = "...";
+var rollBack = "...";
+const fullPrice = "1";
+let adaptive = "...";
+
+console.log("I want to learn JS!");
+
+alert("Hello, world");
