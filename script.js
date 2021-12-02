67b0953087bc01d79d7ff1b10e01020a1a2de199
Gothalion <mbarsukovv@gmail.com>
2021-12-02 23:11:36 +0300

    Added: вывод в консоль используя методы и св-ва

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ let rollBack = 99;
 let fullPrice = 250;
 let adaptive = true;
 
-console.log("I want to learn JS!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-alert("Hello, world");
+console.log(screens.length);
+
+let screenPriceMessage = "Стоимость верстки экранов ";
+let fullPriceMessage = "Стоимость разработки сайта ";
+let priceCurrency = " долларов";
+
+console.log(screenPriceMessage + screenPrice + priceCurrency);
+console.log(fullPriceMessage + fullPrice + priceCurrency);
+
+console.log(screens.toUpperCase());
+console.log(screens.split(", "));
+
+console.log(fullPrice * (rollBack / 100));
