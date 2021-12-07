d73ca29744c766fc00cd7fee3f861f95df1742fc
Gothalion <mbarsukovv@gmail.com>
2021-12-07 23:34:45 +0300

    Feature: added functions Fix: cleared logs

--- script.js
+++ script.js
@@ -8,36 +8,59 @@ let serviceOne = prompt("Какой дополнительный тип услу
 let servicePriceOne = +prompt("Сколько это будет стоить?");
 let serviceTwo = prompt("Какой дополнительный тип услуги нужен?");
 let servicePriceTwo = +prompt("Сколько это будет стоить?");
-let rollBack = 99;
+let rollBack = 10;
+let fullPrice;
+let servicePercentPrice;
+let allServicePrices;
 
-let fullPrice = screenPrice + servicePriceOne + servicePriceTwo;
-let servicePercentPrice = fullPrice - fullPrice * (rollBack / 100);
+const getAllServicePrices = function () {
+  return servicePriceOne + servicePriceTwo;
+};
 
-let screenPriceMessage = "Стоимость верстки экранов ";
-let fullPriceMessage = "Стоимость разработки сайта ";
-let priceCurrency = " долларов";
-
-if (fullPrice >= 30000) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice >= 0 && fullPrice < 15000) {
-  console.log("Скидка не предусмотрена");
-} else {
-  console.log("Что-то пошло не так");
+function getFullPrice() {
+  return screenPrice + allServicePrices;
 }
 
+const getServicePercentPrices = function () {
+  return fullPrice - fullPrice * (rollBack / 100);
+};
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+
+const getTitle = function () {
+  return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
+};
+
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (price >= 15000 && price < 30000) {
+    return "Даем скидку в 5%";
+  } else if (price >= 0 && price < 15000) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что-то пошло не так";
+  }
+};
+
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
-console.log(typeof fullPrice);
+console.log(typeof screenPrice);
 console.log(typeof adaptive);
 
-console.log(Math.ceil(servicePercentPrice));
-console.log(screens.length);
+console.log([screens]);
+console.log(servicePercentPrice);
 
-console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "долларов");
-console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "долларов");
-
-console.log(screens.toUpperCase());
-console.log(screens.split(", "));
-
-console.log(fullPrice * (rollBack / 100));
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
